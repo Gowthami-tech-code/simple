74331ce42a7149b67a76997846e45ce53b526cfa
morthasrikar <srikarm@adityauniversity.in>
2025-12-08 11:56:34 +0530

    mychange

--- simple/script.js
+++ simple/script.js
@@ -2,7 +2,7 @@ const button = document.getElementById("greetBtn");
 const greeting = document.getElementById("greeting");
 
 button.addEventListener("click", () => {
-    const names = ["Chinnathalli", "Alice", "Bob", "Charlie"];
+    const names = ["Chinnathalli", "Alice", "Bob", "Charlie", "Darling"];
     const randomName = names[Math.floor(Math.random() * names.length)];
     greeting.textContent = `Hello, ${randomName}! 👋`;
 });
